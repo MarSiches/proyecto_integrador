77f928cc3008b331330f557e41f9ed120d95e6b2
MarianaElsne <elstnermariana@gmail.com>
2022-06-26 20:20:58 -0300

    - primer prueba styles

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const path= require('path');
 const publicPath= path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-res.send("<h1>Proyecto Hotel Mocona</h1>")
-})
 
 app.get ('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
