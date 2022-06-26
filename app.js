c5f37604848eb19768d427201e51ad571aaf5e54
MarianaElsne <elstnermariana@gmail.com>
2022-06-26 17:18:58 -0300

    Titulo en la pagina

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const publicPath= path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
-res.send("Proyecto Hotel Mocona")
+res.send("<h1>Proyecto Hotel Mocona</h1>")
 })
 
 app.listen(3030,()=>console.log("El servidor 3030 esta corriendo"))
