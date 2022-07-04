15b00b34eb3a03a5c5535f561e47aa9555556cbe
MarianaElsne <elstnermariana@gmail.com>
2022-07-04 09:52:35 -0300

    Agregue turismo accesible ruta

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ res.sendFile(path.resolve(__dirname, "./views/register.html"))
 app.get ('/consultaHabitacion', (req, res) =>{
     res.sendFile(path.resolve(__dirname, "./views/consultaHabitacion.html"))
 });
+app.get ('/turismoAccesible', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/turismoAccesible.html"))
+});
 
 app.get ('/reservaPaso2', (req, res) =>{
     res.sendFile(path.resolve(__dirname, "./views/reservaPaso2.html"))
