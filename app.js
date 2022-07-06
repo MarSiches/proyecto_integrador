9f97925044558e0e70e2314eb5a0d2fc593cc8d0
gabymarx <gmarx31@gmail.com>
2022-07-06 11:03:37 -0300

    Home,menu, pie de pagina, galeria y otros contenidos

--- app.js
+++ app.js
@@ -5,45 +5,30 @@ const path= require('path');
 const publicPath= path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-
 app.get ('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
-})
-
+});
 app.get ('/habitaciones', (req, res) =>{
     res.sendFile(path.resolve(__dirname, "./views/habitaciones.html"))
 });
 app.get ('/servicios', (req, res) =>{
     res.sendFile(path.resolve(__dirname, "./views/servicios.html"))
 });
-app.get ('/login', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+app.get ('/turismo_accesible', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/turismo_accesible.html"))
 });
-app.get ('/register', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/register.html"))
-});
-app.post('/register', (req, res) =>{
-res.sendFile(path.resolve(__dirname, "./views/register.html"))
-}); 
-app.get ('/consultaHabitacion', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/consultaHabitacion.html"))
-});
-// app.get ('/turismoAccesible', (req, res) =>{
-//     res.sendFile(path.resolve(__dirname, "./views/turismoAccesible.html"))
-// });
-app.get ('/politicasReserva', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/politicasReserva.html"))
-});app.get ('/galeria', (req, res) =>{
+app.get ('/galeria', (req, res) =>{
     res.sendFile(path.resolve(__dirname, "./views/galeria.html"))
 });
+// app.get ('/login', (req, res) =>{
+//     res.sendFile(path.resolve(__dirname, "./views/login.html"))
+// });
+// app.get ('/register', (req, res) =>{
+//     res.sendFile(path.resolve(__dirname, "./views/register.html"))
+// });
+// app.post('/register', (req, res) =>{
+// res.sendFile(path.resolve(__dirname, "./views/register.html"))
+// })
 
-app.get ('/reservaPaso2', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/reservaPaso2.html"))
-});
-
-app.get ('/reservaPaso3', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/reservaPaso3.html"))
-});
-
-app.listen(3030,()=>console.log("El servidor 3030 esta corriendo"))
+app.listen(3030,()=>console.log("El servidor 3030 esta corriendo"));
 
